b81904fc13d6b4ce758f0e35448e58d7b1cc1e86
freelance.pg.src <freelance.pg.src@gmail.com>
2020-04-06 00:46:27 +0900

    じゃんけんの結果画面作成

---
+++ app/src/main/java/jp/dagon/firstandroidbook/janke2/ResultActivity.kt
@@ -0,0 +1,12 @@
+package jp.dagon.firstandroidbook.janke2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
