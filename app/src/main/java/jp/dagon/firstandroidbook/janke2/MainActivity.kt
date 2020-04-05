0bc2e5c3cdffab289946e72753cde819d3608da7
freelance.pg.src <freelance.pg.src@gmail.com>
2020-04-06 02:31:01 +0900

    インテントによる画面遷移

--- app/src/main/java/jp/dagon/firstandroidbook/janke2/MainActivity.kt
+++ app/src/main/java/jp/dagon/firstandroidbook/janke2/MainActivity.kt
@@ -1,12 +1,31 @@
 package jp.dagon.firstandroidbook.janke2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gu.setOnClickListener { onJankenButtonTapped(it) }
+        choki.setOnClickListener { onJankenButtonTapped(it) }
+        pa.setOnClickListener { onJankenButtonTapped(it) }
+    }
+
+    /**
+     * ボタンを押した時に実行する
+     */
+    fun onJankenButtonTapped(view: View?) {
+        // Intentクラスのコンストラクタ(context:呼び出し元のインスタンス class:呼び出したいアクティビティのクラス)
+        val intent = Intent(this, ResultActivity::class.java)
+        // インテントにデータを格納(name:追加したい情報キー value:追加する値)
+        intent.putExtra("MY_HAND", view?.id)
+        // アクティビティを起動する
+        startActivity(intent)
     }
 }
--- app/src/main/java/jp/dagon/firstandroidbook/janke2/ResultActivity.kt
+++ app/src/main/java/jp/dagon/firstandroidbook/janke2/ResultActivity.kt
@@ -2,11 +2,64 @@ package jp.dagon.firstandroidbook.janke2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
+
+    val gu = 0
+    val choki = 1
+    val pa = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        // インテントから追加情報を取り出す(name:取り出したい情報キー defaultValue:データーが取り出せなかった時のデフォルト値)
+        val id = intent.getIntExtra("My_HAND", 0)
+
+        val myHand: Int
+
+        myHand = when (id) {
+            R.id.gu -> {
+                // ImmageViewのコンテンツに画像リソースを設定(resId:画像のリソースID)
+                myHandImage.setImageResource(R.drawable.gu)
+                gu
+            }
+            R.id.choki -> {
+                myHandImage.setImageResource(R.drawable.choki)
+                choki
+            }
+            R.id.pa -> {
+                myHandImage.setImageResource(R.drawable.pa)
+                pa
+            }
+            else -> gu
+        }
+
+        // コンピュータの手を決める
+        val comHand = (Math.random() * 3).toInt()
+
+        when (comHand) {
+            gu -> comHandImage.setImageResource(R.drawable.com_gu)
+            choki -> comHandImage.setImageResource(R.drawable.com_choki)
+            pa -> comHandImage.setImageResource(R.drawable.com_pa)
+        }
+
+        // 勝敗を判定する
+        val gameResult = (comHand - myHand + 3) % 3
+
+        when (gameResult) {
+            // 引き分け
+            0 -> resultLabel.setText(R.string.result_draw)
+            // 勝った場合
+            1 -> resultLabel.setText(R.string.result_win)
+            // 負けた場合
+            2 -> resultLabel.setText(R.string.result_lose)
+        }
+
+        backButton.setOnClickListener {
+            // 現在のアクティビティを終了し前のアクティビティに戻る
+            finish()
+        }
     }
 }
